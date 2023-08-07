eb26cae1e67b1ab0407bffdda193f0ac400fa7ef
Ryan Augustyn <jraugustyn07@gmail.com>
2023-08-07 16:15:18 -0500

    ADD hero section

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Hero from "../../components/Hero/Hero";
 
 const LandingPage = () => {
   return (
     <div>
       <h1 className="landingPage">LandingPage</h1>
       <p>Sunny Software</p>
+      <Hero></Hero>
     </div>
   );
 };
