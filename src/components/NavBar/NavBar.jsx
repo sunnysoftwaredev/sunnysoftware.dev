2d9b881b0eb75c2f14bda84039220f175813615e
Ryan Augustyn <jraugustyn07@gmail.com>
2023-08-07 18:33:50 -0500

    ADD css & bootstrap styling to NavBar and Footer

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,58 +1,58 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
-
 const Navbar = () => {
   return (
-    <div className="navBar">
-      <ul>
-        <li className="home">
-          <Link to="/">
-            <b>Sunny Software</b>
-          </Link>
-        </li>
-        <li className="login">
-          <Link to="/login">
-            <b>Login Page</b>
-          </Link>
-        </li>
-        <li className="contact">
-          <Link to="/contact-us">
-            <b>Contact Us</b>
-          </Link>
-        </li>
-        <li className="getStarted">
-          <Link to="/get-started">
-            <b> Get Started</b>
-          </Link>
-        </li>
-        <li className="clientPortal">
-          <Link to="/portal">
-            <b> Client Portal</b>
-          </Link>
-        </li>
-        <li className="portal">
-          <Link to="/work-portal">
-            <b> Work Portal</b>
-          </Link>
-        </li>
-        <li className="aboutUs">
-          <Link to="/about-us">
-            <b> About Us</b>
-          </Link>
-        </li>
-        <li className="services">
-          <Link to="/services">
-            <b> Services</b>
-          </Link>
-        </li>
-        <li className="portfolio">
-          <Link to="/portfolio">
-            <b> Portfolio</b>
-          </Link>
-        </li>
-      </ul>
-    </div>
+    <nav className="navbar navbar-expand-md bg-light navbar-light">
+      <div className="container-fluid">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" href="/about-us">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a class="nav-link active" href="/login">
+              Login
+            </a>
+          </li>
+          <li className="nav-item">
+            <a class="nav-link active" href="/contact-us">
+              Contact Us
+            </a>
+          </li>
+          <li className="nav-item">
+            <a class="nav-link active" href="/get-started">
+              Get Started
+            </a>
+          </li>
+          <li className="nav-item">
+            <a class="nav-link active" href="/portal">
+              Client Portal
+            </a>
+          </li>
+          <li className="nav-item">
+            <a class="nav-link active" href="/work-portal">
+              Work Portal
+            </a>
+          </li>
+          <li className="nav-item">
+            <a class="nav-link active" href="/about-us">
+              About Us
+            </a>
+          </li>
+          <li className="nav-item">
+            <a class="nav-link active" href="/services">
+              Services
+            </a>
+          </li>
+          <li className="nav-item">
+            <a class="nav-link active" href="/portfolio">
+              Portfolio
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
