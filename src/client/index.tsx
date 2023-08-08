2b37bdf2058c8ed8122049409bfc580cf6b59600
Trevin Hofmann <trevinhofmann@gmail.com>
2023-08-07 23:59:20 -0700

    feat: switch to typescript and our own webpack config

---
+++ src/client/index.tsx
@@ -0,0 +1,20 @@
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.scss";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+
+if (rootElement === null) {
+  throw new Error('Unexpected null root element');
+}
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </StrictMode>
+);
