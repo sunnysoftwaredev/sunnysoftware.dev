3da3ba1112aca8926420c0c84abdbfc46f0379cd
Ryan Augustyn <jraugustyn07@gmail.com>
2023-08-09 10:33:21 -0500

    add: styling to NavBar and pin Footer to bottom

--- src/client/components/NavBar/NavBar.tsx
+++ src/client/components/NavBar/NavBar.tsx
@@ -1,57 +1,56 @@
 import React from "react";
 import type { FunctionComponent } from "react";
+import "./NavBar.scss";
 
 const Navbar: FunctionComponent = () => (
-  <nav className="navbar navbar-expand-md bg-light navbar-light">
-    <div className="container-fluid">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" href="/">
-            Home
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" href="/login">
-            Login
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" href="/contact-us">
-            Contact Us
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" href="/get-started">
-            Get Started
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" href="/portal">
-            Client Portal
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" href="/work-portal">
-            Work Portal
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" href="/about-us">
-            About Us
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" href="/services">
-            Services
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" href="/portfolio">
-            Portfolio
-          </a>
-        </li>
-      </ul>
-    </div>
+  <nav className="navbar">
+    <ul className="navbarUL">
+      <li className="nav-item">
+        <a className="nav-link active" href="/">
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link active" href="/login">
+          Login
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link active" href="/contact-us">
+          Contact Us
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link active" href="/get-started">
+          Get Started
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link active" href="/portal">
+          Client Portal
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link active" href="/work-portal">
+          Work Portal
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link active" href="/about-us">
+          About Us
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link active" href="/services">
+          Services
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link active" href="/portfolio">
+          Portfolio
+        </a>
+      </li>
+    </ul>
   </nav>
 );
 
