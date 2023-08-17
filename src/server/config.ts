5789969a4e5e9e2927bc4f04ee718109e3b67dcd
Trevin Hofmann <trevinhofmann@gmail.com>
2023-08-16 18:37:40 -0700

    feat: update configuration, use pg package for connecting to the database

---
+++ src/server/config.ts
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const getEnvironmentVariable = (key: string): string => {
+  const value = process.env[key];
+  if (value === undefined) {
+    throw new Error(`Missing environment variable: ${key}`);
+  }
+  return value;
+};
+
+const getNumericEnvironmentVariable = (key: string): number => {
+  const value = getEnvironmentVariable(key);
+  const number = Number.parseInt(value, 10);
+  if (Number.isNaN(number)) {
+    throw new Error(`Environment variable "${key}" must be a number`);
+  }
+  return number;
+};
+
+export default {
+  isProduction: getEnvironmentVariable('NODE_ENV') === 'production',
+  pg: {
+    host: getEnvironmentVariable('PG_HOST'),
+    port: getNumericEnvironmentVariable('PG_PORT'),
+    database: getEnvironmentVariable('PG_DATABASE'),
+    user: getEnvironmentVariable('PG_USER'),
+    password: getEnvironmentVariable('PG_PASSWORD'),
+  },
+};
---
+++ src/server/server.ts
@@ -0,0 +1,28 @@
+import path from 'path';
+import express from 'express';
+import logger from './logger';
+import api from './api';
+
+export const initializeServer = async(): Promise<void> => (
+  new Promise((resolve) => {
+    const PORT = 3000;
+
+    const app = express();
+
+    const clientDirectory = path.join(__dirname, './client');
+    const indexPage = path.join(clientDirectory, 'index.html');
+
+    app.use(express.static(clientDirectory));
+
+    app.use('/api', api);
+
+    app.get('*', (req, res) => {
+      res.sendFile(indexPage);
+    });
+
+    app.listen(PORT, () => {
+      logger.info(`Server is listening on port ${PORT}`);
+      resolve();
+    });
+  })
+);
