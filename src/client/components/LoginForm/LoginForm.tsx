b5507623be61b8010037c6c6b3837eebaa0f1588
Ryan Augustyn <jraugustyn07@gmail.com>
2023-08-28 12:47:46 -0500

    WIP add and remove body parser, webpack not working, add some function for login

--- src/client/components/LoginForm/LoginForm.tsx
+++ src/client/components/LoginForm/LoginForm.tsx
@@ -1,14 +1,31 @@
 import React, { useState, useCallback } from 'react';
-import type { FunctionComponent } from 'react';
+import { redirect } from 'react-router-dom';
+import type { FunctionComponent, ChangeEvent } from 'react';
+import { isObjectRecord } from '../../../server/database';
 
 const LoginForm: FunctionComponent = () => {
+  // check if user logged in already
+  const user = localStorage.getItem('user');
+  if (user !== null) {
+    redirect('/http://127.0.0.1/3000/');
+  }
+
+  // const [error, setError] = useState<string | undefined>(undefined);
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
-  const handleUsernameChange = useCallback((event:
-  ChangeEventHandler<HTMLInputElement>):
-  void => setUsername(event.value), [setUsername]);
-  const handlePasswordChange = useCallback((newValue: string):
-  void => setPassword(newValue), [setPassword]);
+  const handleUsernameChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setUsername(e.target.value);
+    },
+    [setUsername],
+  );
+
+  const handlePasswordChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setPassword(e.target.value);
+    },
+    [setPassword],
+  );
 
   const handleSubmit = useCallback(async() => {
     try {
@@ -23,49 +40,68 @@ const LoginForm: FunctionComponent = () => {
         }),
       });
 
-      const result = await response.json();
-    } catch (error) {
-
+      const result: unknown = await response.json();
+      if (!isObjectRecord(result)) {
+        throw new Error('Unexpected body type: LoginForm.tsx');
+      }
+      if (typeof result.success !== 'boolean') {
+        throw new Error('success variable not type boolean: LoginForm.tsx');
+      }
+      if (typeof result.isloggedin !== 'boolean') {
+        throw new Error('isloggedin variable not type boolean: LoginForm.tsx');
+      }
+      // store string in localStorage
+      if (typeof result.username !== 'string') {
+        throw new Error('username variable not type string: LoginForm.tsx');
+      }
+      if (result.success) {
+        if (result.isloggedin) {
+          localStorage.setItem('user', JSON.stringify(response.body));
+          redirect('/http://127.0.0.1/3000/');
+        } else {
+          // setError('username or password does not match in LoginForm.tsx');
+          // commented out error variable not used
+        }
+      }
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        // something here
+      }
     }
   }, [username, password]);
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="form-outline mb-4">
+      <div className="usernameBox">
         <input
-          type="email" id="username" className="form-control"
+          type="text" id="username" className="usernameBox"
           onChange={handleUsernameChange}
         />
-        <label className="form-label" htmlFor="username">
+        <label className="usernameLabel" htmlFor="username">
           Username
         </label>
       </div>
 
-      <div className="form-outline mb-4">
+      <div className="passwordBox">
         <input
-          type="password" id="passwordBox" className="form-control"
+          type="password" id="passwordBox" className="passwordBox"
           onChange={handlePasswordChange}
         />
-        <label className="form-label" htmlFor="passwordBox">
+        <label className="passwordLabel" htmlFor="passwordBox">
           Password
         </label>
       </div>
 
-      <div className="row mb-4">
-        <div className="col">
-          <a href="#">Forgot password?</a>
-        </div>
+      <div className="forgotPassword">
+        <a href="#">Forgot password?</a>
       </div>
 
-      <button type="button" className="btn btn-primary btn-block mb-4">
+      <button type="button" className="loginButton">
         Sign in
       </button>
 
-      <div className="text-center">
-        <p>
-          <a href="#!">Create an account</a>
-        </p>
-
+      <div className="createAccount">
+        <a href="#!">Create an account</a>
       </div>
     </form>
   );
