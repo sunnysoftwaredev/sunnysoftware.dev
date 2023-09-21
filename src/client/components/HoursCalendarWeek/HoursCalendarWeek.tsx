65864205c6abf4d90769add299f8e87756d51d6e
Ryan Augustyn <jraugustyn07@gmail.com>
2023-09-21 10:05:21 -0500

    remove ^ chars in package.json, add styling and functionality to standalone week calendar, including ability to switch weeks and ability to click on days and get back string of date

--- src/client/components/HoursCalendarWeek/HoursCalendarWeek.tsx
+++ src/client/components/HoursCalendarWeek/HoursCalendarWeek.tsx
@@ -1,45 +1,128 @@
 import type { FunctionComponent } from 'react';
-import React from 'react';
+import React, { useCallback, useContext, useState } from 'react';
+import AuthContext from '../../context/AuthContext';
+import logger from '../../../server/logger';
 import styles from './HoursCalendarWeek.scss';
 
-const HoursCalendarWeek: FunctionComponent = ({ selectedDate }) => {
-  const displayWeek = () => {
-    const daysInWeek = [];
-    for (let i = 0; i < 7; i++) {
-      daysInWeek.push(<div key={`weekDay-${i}`} className={styles.weekDay}>
-        {' '}
-        {i}
-        {' '}
+const HoursCalendarWeek: FunctionComponent = () => {
+  const { username } = useContext(AuthContext) ?? { username: 'loading' };
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [clickedDate, setClickedDate] = useState('');
 
-      </div>);
+  const getDaysInWeek = (): Date[] => {
+    const days: Date[] = [];
+    for (let i = 0; i < 7; i++) {
+      const first = currentDate.getDate() - currentDate.getDay() + i;
+      const day
+      = new Date(currentDate.setDate(first));
+      days.push(day);
     }
-    return daysInWeek;
+    return days;
+  };
+
+  const daysInWeek = getDaysInWeek();
+
+  const changeToPrevWeek = useCallback((): void => {
+    setCurrentDate((currDate: Date): Date => {
+      const Year = currDate.getFullYear();
+      const Month = currDate.getMonth();
+      const Day = currDate.getDate();
+      return new Date(Year, Month, Day - 7);
+    });
+  }, []);
+
+  const changeToNextWeek = useCallback((): void => {
+    setCurrentDate((currDate: Date): Date => {
+      const Year = currDate.getFullYear();
+      const Month = currDate.getMonth();
+      const Day = currDate.getDate();
+      return new Date(Year, Month, Day + 7);
+    });
+  }, []);
+
+  const handleDateClick
+   = useCallback((e: React.MouseEvent<HTMLDivElement>): void => {
+     e.preventDefault();
+     const { target } = e;
+     if (target instanceof HTMLDivElement) {
+       const dayString: string = target.innerText;
+       setClickedDate(dayString);
+     } else {
+       logger.info('type error in handleDateClick');
+     }
+   }, []);
+
+  const displayWeek = (): React.ReactElement[] => {
+    const dayObjects: React.ReactElement[] = [];
+    for (let i = 0; i < 7; i++) {
+      const rawDate = daysInWeek[i];
+      const day: string = rawDate.toString().slice(0, 10);
+      dayObjects.push(<div
+        key={`day-${i}`}
+        className={styles.box}
+        onClick={handleDateClick}
+      >
+        {day}
+                      </div>);
+    }
+    return dayObjects;
   };
 
   return (
     <div>
-      <h3>Week of </h3>
-      <div className={styles.header}>
-        <button type="button" > BACK </button>
-        <h2>
-          {selectedDate.toLocaleString('default', {
-            month: 'long',
-            year: 'numeric',
-          })}
-        </h2>
-        <button type="button" > NEXT </button>
-      </div>
-      <div className={styles.headerDays}>
-        <div>Su</div>
-        <div>Mo</div>
-        <div>Tu</div>
-        <div>We</div>
-        <div>Th</div>
-        <div>Fr</div>
-        <div>Sa</div>
-      </div>
-      <div className={styles.weekBody}>
-        {displayWeek()}
+      <h3>
+        Work hours for
+        {' '}
+        {username}
+        {' '}
+
+      </h3>
+
+      <div className={styles.week}>
+        <div className={styles.header}>
+          <button type="button" onClick={changeToPrevWeek}> BACK </button>
+          <h2>
+            Week of
+            {' '}
+            {' '}
+            {daysInWeek[0].toLocaleString('default', {
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+            })}
+            {' '}
+            {' '}
+            to
+            {' '}
+            {' '}
+            {daysInWeek[6].toLocaleString('default', {
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+            })}
+          </h2>
+          <button type="button" onClick={changeToNextWeek}> NEXT </button>
+        </div>
+        <div className={styles.headerDays}>
+          <div>Su</div>
+          <div>Mo</div>
+          <div>Tu</div>
+          <div>We</div>
+          <div>Th</div>
+          <div>Fr</div>
+          <div>Sa</div>
+        </div>
+        <div className={styles.body}>
+          {displayWeek()}
+          {' '}
+        </div>
+
+        <div className={styles.boxSelected}>
+          Selected Date:
+          {' '}
+          <p>{clickedDate}</p>
+        </div>
+
       </div>
     </div>
   );
