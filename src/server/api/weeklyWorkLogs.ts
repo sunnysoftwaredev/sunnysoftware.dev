7ab96cbb4ab5d6c6807a39e8b1fdcc1ae8601b9a
Ryan Augustyn <jraugustyn07@gmail.com>
2023-10-13 10:32:22 -0500

    add async-mutex package and lock to api, add the word Work to weeklyWorkLogs so connection with workLogs is clearer

--- src/server/api/weeklyWorkLogs.ts
+++ src/server/api/weeklyWorkLogs.ts
@@ -1,11 +1,13 @@
 import { Router as createRouter } from 'express';
 // import logger from '../logger';
+import { Mutex } from 'async-mutex';
 import { isObjectRecord } from '../../common/utilities/types';
 import { getIDWithToken, getWeeklyLogs } from '../database';
 import { createTimesheet } from '../common/utilities/createTimesheet';
 import logger from '../logger';
 
 const router = createRouter();
+const mutex = new Mutex();
 
 router.post('/', (req, res) => {
   (async(): Promise<void> => {
@@ -21,36 +23,39 @@ router.post('/', (req, res) => {
       throw new Error('api/weeklyLogs: userToken not type string');
     }
 
-    const idResult = getIDWithToken(authenticationToken);
-    const id = await idResult;
+    const release = await mutex.acquire();
+    try {
+      const idResult = getIDWithToken(authenticationToken);
+      const userId = await idResult;
 
-    const { unixWeekStart } = req.body;
-    const { unixWeekEnd } = req.body;
+      const { unixWeekStart } = req.body;
+      const { unixWeekEnd } = req.body;
 
-    if (typeof unixWeekStart !== 'number') {
-      throw new Error('api/weeklyLogs.post: unixStart is not number');
-    }
-    if (typeof unixWeekEnd !== 'number') {
-      throw new Error('api/weeklyLogs.post: unixEnd is not number');
-    }
-
-    await createTimesheet(id, unixWeekStart, unixWeekEnd).catch((err) => {
-      if (err instanceof Error) {
-        logger.error(err.message);
+      if (typeof unixWeekStart !== 'number') {
+        throw new Error('api/weeklyLogs.post: unixStart is not number');
       }
-    });
+      if (typeof unixWeekEnd !== 'number') {
+        throw new Error('api/weeklyLogs.post: unixEnd is not number');
+      }
 
-    const result = await getWeeklyLogs(
-      id,
-      unixWeekStart,
-      unixWeekEnd,
-    );
+      await createTimesheet(userId, unixWeekStart, unixWeekEnd).catch((err) => {
+        if (err instanceof Error) {
+          logger.error(err.message);
+        }
+      });
 
-    res.json({
-      success: true,
-      listResult: result,
-    });
-
+      const result = await getWeeklyLogs(
+        userId,
+        unixWeekStart,
+        unixWeekEnd,
+      );
+      res.json({
+        success: true,
+        listResult: result,
+      });
+    } finally {
+      release();
+    }
     // logger.info('res.json success in weeklyLogs.ts post');
   })().catch((e: Error) => {
     res.json({
