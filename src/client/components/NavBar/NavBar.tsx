a7c99f79cba1130a4d0c19c560634ba0125a5b6d
Ryan Augustyn <jraugustyn07@gmail.com>
2023-11-08 13:40:47 -0600

    WIP commit: added some temporary styling to NavBar to test breakpoint, added logo to Assets folder

--- src/client/components/NavBar/NavBar.tsx
+++ src/client/components/NavBar/NavBar.tsx
@@ -1,8 +1,10 @@
 import React, { useCallback, useContext } from 'react';
-import type { FunctionComponent } from 'react';
+import type { FunctionComponent, SyntheticEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AuthContext from '../../context/AuthContext';
 import Button, { ButtonSize } from '../Button/Button';
+import logger from '../../../server/logger';
+import Logo from '../../SCSS/Assets/Logo.png';
 import styles from './NavBar.scss';
 
 const Navbar: FunctionComponent = () => {
@@ -28,78 +30,93 @@ const Navbar: FunctionComponent = () => {
     }
   }, [navigate, active]);
 
+  const handleLetsTalk = useCallback((e: SyntheticEvent): void => {
+    try {
+      e.preventDefault();
+      // setTimeout(() => console.log('timeout'), 3000);
+      navigate('/contact-us');
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        logger.error(err.message);
+      }
+    }
+  }, [navigate]);
   return (
     <nav>
-      <div className="logo">SUNNY SOFTWARE PLACEHOLDER</div>
+      <img src={Logo} alt="Sunny Software Logo" />
       <ul className={styles.menu}>
-        <li className="nav-item">
+        <li className={styles.navItem}>
           <a className="nav-link active" href="/">
             Home
           </a>
         </li>
-        {!active && (
-          <li className="nav-item">
-            <a className="nav-link active" href="/login">
-              Login
-            </a>
-          </li>
-        )}
-        <li className="nav-item">
-          <a className="nav-link active" href="/contact-us">
-            Contact Us
+        <li className={styles.navItem}>
+          <a className="nav-link active" href="/about-us">
+            About Us
           </a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link active" href="/get-started">
-            Get Started
+        <li className={styles.navItem}>
+          <a className="nav-link active" href="/services">
+            Services
+          </a>
+        </li>
+        <li className={styles.navItem}>
+          <a className="nav-link active" href="/portfolio">
+            Portfolio
+          </a>
+        </li>
+        <li className={styles.navItem}>
+          <a className="nav-link active" href="/team">
+            Team
+          </a>
+        </li>
+        <li className={styles.navItem}>
+          <a className="nav-link active" href="/methodology">
+            Methodology
+          </a>
+        </li>
+        <li className={styles.navItem}>
+          <a className="nav-link active" href="/contact-us">
+            Contact
           </a>
         </li>
         {role === 'client' && (
-          <li className="nav-item">
+          <li className={styles.navItem}>
             <a className="nav-link active" href="/portal">
               Client Portal
             </a>
           </li>
         )}
         {role === 'admin' && (
-          <li className="nav-item">
+          <li className={styles.navItem}>
             <a className="nav-link active" href="/admin-portal">
               Admin Portal
             </a>
           </li>
         )}
         {role === 'employee' && (
-          <li className="nav-item">
+          <li className={styles.navItem}>
             <a className="nav-link active" href="/work-portal">
               Work Portal
             </a>
           </li>
         )}
-        <li className="nav-item">
-          <a className="nav-link active" href="/about-us">
-            About Us
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" href="/services">
-            Services
-          </a>
-        </li>
-        {role === 'employee' && (
-          <li className="nav-item">
-            <a className="nav-link active" href="/portfolio">
-              Portfolio
-            </a>
-          </li>
-        )}
       </ul>
-      {active
-        ? (
-          <Button size={ButtonSize.Large} onClick={handleSubmit} variant="outlined">Log Out</Button>
-        )
-        : (
-          <Button size={ButtonSize.Large} variant="outlined" onClick={handleSubmit}>Log In</Button>
-        )}
+      <div className={styles.buttons}>
+        {active
+          ? (
+            <Button size={ButtonSize.Large} onClick={handleSubmit} variant="outlined">Log Out</Button>
+          )
+          : (
+            <Button size={ButtonSize.Large} variant="outlined" onClick={handleSubmit}>Log In</Button>
+          )}
+        <Button
+          size={ButtonSize.Large}
+          onClick={handleLetsTalk}
+        >
+          Let's Talk
+        </Button>
+      </div>
     </nav>
   );
 };
