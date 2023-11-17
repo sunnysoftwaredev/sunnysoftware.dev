b2749d5f0101773704afd8c70d786a3b6cdef425
Ryan Augustyn <jraugustyn07@gmail.com>
2023-11-17 09:25:26 -0600

    Recreate hero with CTA and image, add css for desktop and mobile, changed NavBar background (but not dropdown yet)

--- src/client/components/Hero/Hero.tsx
+++ src/client/components/Hero/Hero.tsx
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import type { FunctionComponent } from 'react';
-import './Hero.scss';
+import { useNavigate } from 'react-router-dom';
+import Button, { ButtonSize } from '../Button/Button';
+import logger from '../../../server/logger';
+import styles from './Hero.scss';
+import heroPicture from './HeroPicture.png';
 
-const Hero: FunctionComponent = () => (
-  <div className="hero">
-    <div className="heroLeftPlaceholder" />
-    <div className="heroRightSide">
-      <h1 className="heroHeading">Sunny Software</h1>
-      <p className="heroSubHeading">
-        Expert engineering from the sunny California Valley
-      </p>
-      <a href="/get-started" className="btn btn-primary">
-        Get Started Today!
-      </a>
+const Hero: FunctionComponent = () => {
+  const navigate = useNavigate();
+
+  const handleClick = useCallback((): void => {
+    try {
+      navigate('/contact-us');
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        logger.error(err.message);
+      }
+    }
+  }, [navigate]);
+  return (
+    <div className={styles.fullHeroContainer}>
+      <div className={styles.hero}>
+        <div className={styles.textContainer}>
+          <h1>Journey into the tech of tomorrow</h1>
+          <p>
+            Step into the future with our cutting-edge
+            technology solutions tailored for your business needs
+          </p>
+          <Button size={ButtonSize.Large} onClick={handleClick} >
+            Get a Quote
+          </Button>
+        </div>
+        <div className={styles.imgContainer}>
+          <img src={heroPicture} className={styles.heroImg} alt="Two people happy with Sunny Software" />
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Hero;
