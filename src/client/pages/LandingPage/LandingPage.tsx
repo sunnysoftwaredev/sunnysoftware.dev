544190748aa546dbd7a85d40374bc9e20817ab7c
Ryan Augustyn <jraugustyn07@gmail.com>
2023-11-29 10:54:00 -0600

    Recreate CallToAction component

--- src/client/pages/LandingPage/LandingPage.tsx
+++ src/client/pages/LandingPage/LandingPage.tsx
@@ -3,7 +3,7 @@ import type { FunctionComponent } from 'react';
 import { Helmet } from 'react-helmet';
 import Hero from '../../components/Hero/Hero';
 import Services from '../../components/Services/Services';
-// import CallToAction from '../../components/CallToAction/CallToAction';
+import CallToAction from '../../components/CallToAction/CallToAction';
 import Methodology from '../../components/Methodology/Methodology';
 import PortfolioCarousel from '../../components/PortfolioCarousel/PortfolioCarousel';
 import OurTeamAndOpenings from '../../components/OurTeamAndOpenings/OurTeamAndOpenings';
@@ -27,7 +27,7 @@ const LandingPage: FunctionComponent = () => (
     <EmployeeCardStack />
     <FrequentlyAskedQuestions />
     <ResourcesPanel />
-    {/* <CallToAction /> */}
+    <CallToAction />
   </div>
 );
 
