83dddd8e590a700845eeefd0da4654c985ed42d4
Ryan Augustyn <jraugustyn07@gmail.com>
2023-11-30 12:30:03 -0600

    Remove placeholder line from LocationBanner.tsx

--- src/client/components/AboutUsPage/LocationBanner/LocationBanner.tsx
+++ src/client/components/AboutUsPage/LocationBanner/LocationBanner.tsx
@@ -3,18 +3,15 @@ import React from 'react';
 import styles from './LocationBanner.scss';
 import buildingImage from './buildingImage.png';
 
-const LocationBanner: FunctionComponent = () => {
-  const x = 1;
-  return (
-    <div className={styles.container}>
-      <img className={styles.buildingImage} src={buildingImage} alt="Office building" />
-      <div className={styles.text}>
-        <h2>Where we are based</h2>
-        <h3>Sunnyvale, CA</h3>
-        <p>+93830903903</p>
-      </div>
+const LocationBanner: FunctionComponent = () => (
+  <div className={styles.container}>
+    <img className={styles.buildingImage} src={buildingImage} alt="Office building" />
+    <div className={styles.text}>
+      <h2>Where we are based</h2>
+      <h3>Sunnyvale, CA</h3>
+      <p>+93830903903</p>
     </div>
-  );
-};
+  </div>
+);
 
 export default LocationBanner;
