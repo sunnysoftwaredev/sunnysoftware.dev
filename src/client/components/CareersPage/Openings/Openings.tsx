8175d1e6977761db741f234aaec27239b783aa5b
Trevin Hofmann <trevinhofmann@gmail.com>
2023-12-07 17:41:40 -0800

    refactor: lint fixes for Openings

--- src/client/components/CareersPage/Openings/Openings.tsx
+++ src/client/components/CareersPage/Openings/Openings.tsx
@@ -1,40 +1,27 @@
-import React, { useCallback } from 'react';
-import { useNavigate } from 'react-router-dom';
-import logger from '../../../../server/logger';
+import React from 'react';
 import styles from './Openings.scss';
 
-const Openings: React.FunctionComponent = () => {
-  const navigate = useNavigate();
-  const getAQuoteClick = useCallback(() => {
-    try {
-      navigate('./contact-us');
-    } catch (err: unknown) {
-      if (err instanceof Error) {
-        logger.error(err.message);
-      }
-    }
-  }, [navigate]);
+// const OPENINGS = [
+//   {
+//     position: 'Digital Senior Designer',
+//     type: 'remote',
+//     url: './job-link',
+//     description: 'Description of job requirements',
+//   },
+// ];
 
-  const openings = [
-    { position: 'Digital Senior Designer',
-      type: 'remote',
-      url: './job-link',
-      description: 'Description of job requirements , Description of job requirementsDescription of job requirements' },
-  ];
-  return (
-    <div className={styles.container}>
-      <div className={styles.text}>
-        <h2>Our team of professionals</h2>
-        <p>
-          Our mission is to become an extension of
-          your team so we can help your business grow
-          — all while costing you less than a single
-          full-time designer.
-          {' '}
-        </p>
-      </div>
+const Openings: React.FunctionComponent = () => (
+  <div className={styles.container}>
+    <div className={styles.text}>
+      <h2>Our team of professionals</h2>
+      <p>
+        Our mission is to become an extension of
+        your team so we can help your business grow
+        — all while costing you less than a single
+        full-time designer.
+      </p>
     </div>
-  );
-};
+  </div>
+);
 
 export default Openings;
