c29082111b8351b78cfc8f0c7b0d0c4e58dee5a0
Ryan Augustyn <jraugustyn07@gmail.com>
2023-12-11 10:19:58 -0600

    Create OwnIdeasBottom component on CareersPage, fix scss  selection issue with CareersBanner at top

---
+++ src/client/components/CareersPage/OwnIdeasBottom/OwnIdeasBottom.tsx
@@ -0,0 +1,22 @@
+import type { FunctionComponent } from 'react';
+import React from 'react';
+import Button, { ButtonSize, ButtonVariant } from '../../Button/Button';
+import styles from './OwnIdeasBottom.scss';
+
+const OwnIdeasBottom: FunctionComponent = () => (
+  <div className={styles.container}>
+    <div className={styles.text}>
+      <h2>Have your own ideas for our partnership?</h2>
+      <p>
+        Our mission is to become an extension of your
+        team so we can help your business grow — all
+        while costing you less than a single full-time designer.
+      </p>
+    </div>
+    <Button size={ButtonSize.Large} variant={ButtonVariant.Outlined} to="./contact-us">
+      Get a Quote
+    </Button>
+  </div>
+);
+
+export default OwnIdeasBottom;
