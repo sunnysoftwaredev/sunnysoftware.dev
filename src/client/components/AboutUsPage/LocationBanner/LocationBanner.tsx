1f6aa9aa7a5ce139d48a781e7a23f40f636f1961
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-01-05 06:08:25 +0000

    Separated address part into a new 'Address' component for better code structuring and readability.

--- src/client/components/AboutUsPage/LocationBanner/LocationBanner.tsx
+++ src/client/components/AboutUsPage/LocationBanner/LocationBanner.tsx
@@ -3,14 +3,18 @@ import React from 'react';
 import styles from './LocationBanner.scss';
 import buildingImage from './buildingImage.png';
 
+const Address: FunctionComponent = () => (
+  <div className={styles.text}>
+    <h2>Where we are based</h2>
+    <h3>Sunnyvale, CA</h3>
+    <p>+93830903903</p>
+  </div>
+);
+
 const LocationBanner: FunctionComponent = () => (
   <div className={styles.container}>
     <img className={styles.buildingImage} src={buildingImage} alt="Office building" />
-    <div className={styles.text}>
-      <h2>Where we are based</h2>
-      <h3>Sunnyvale, CA</h3>
-      <p>+93830903903</p>
-    </div>
+    <Address />
   </div>
 );
 
