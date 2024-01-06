99bec19251298f6b6a6baa51b804950e0a0c09ac
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-01-06 00:28:52 +0000

    refactor: simplify popup state management in 'ResetPassword' component

--- src/client/components/LoginPage/ResetPassword/ResetPassword.tsx
+++ src/client/components/LoginPage/ResetPassword/ResetPassword.tsx
@@ -14,73 +14,46 @@ const ResetPassword: FunctionComponent = () => {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
 
-  const [showSuccessPopup, setShowSuccessPopup] = useState(false);
-  const [showErrorPopup, setShowErrorPopup] = useState(false);
-  const [showMismatchPopup, setShowMismatchPopup] = useState(false);
+  const [popupStates, setPopupStates] = useState({
+    showSuccessPopup: false,
+    showErrorPopup: false,
+    showMismatchPopup: false
+  });
 
   const navigate = useNavigate();
-
   const [param] = useSearchParams();
   const resetToken = param.get('token');
 
-  const { active }
-  = useContext(AuthContext) ?? { active: false };
+  const { active } = useContext(AuthContext) ?? { active: false };
 
   if (active) {
     navigate('/');
   }
 
-  const handleNewPasswordChange = useCallback(
+  const handlePasswordChange = (setter) => useCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
-      setNewPassword(e.target.value);
+      setter(e.target.value);
     },
-    [setNewPassword],
+    [setter],
   );
 
-  const handleConfirmPasswordChange = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      setConfirmPassword(e.target.value);
-    },
-    [setConfirmPassword],
-  );
+  const handleShowPasswordChange = useCallback(() => {
+    setShowPassword(!showPassword);
+  }, [setShowPassword, showPassword]);
 
-  const handleShowPasswordChange = useCallback(
-    () => {
-      setShowPassword(!showPassword);
-    },
-    [setShowPassword, showPassword],
-  );
+  const togglePopup = (popupKey: keyof typeof popupStates) => useCallback(() => {
+    setPopupStates(prevState => ({ ...prevState, [popupKey]: !prevState[popupKey] }));
+  }, []);
 
-  const showSuccessPopupFunction = useCallback(() => {
-    setShowSuccessPopup(!showSuccessPopup);
-  }, [showSuccessPopup]);
-
-  const showErrorPopupFunction = useCallback(() => {
-    setShowErrorPopup(!showErrorPopup);
-  }, [showErrorPopup]);
-
-  const showMismatchPopupFunction = useCallback(() => {
-    setShowMismatchPopup(!showMismatchPopup);
-  }, [showMismatchPopup]);
-
-  const comparePasswords = useCallback(
-    (
-      password1: string,
-      password2: string
-    ): boolean => {
-      if (password1 !== password2) {
-        return false;
-      }
-      return true;
-    },
-    [],
-  );
+  const comparePasswords = (password1: string, password2: string): boolean => {
+    return password1 === password2;
+  };
 
   const handleSubmit = useCallback(async(e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const passwordsSame = comparePasswords(newPassword, confirmPassword);
     if (!passwordsSame) {
-      setShowMismatchPopup(true);
+      setPopupStates({...popupStates, showMismatchPopup: true});
       return;
     }
     try {
@@ -104,19 +77,20 @@ const ResetPassword: FunctionComponent = () => {
         throw new Error('success variable not type boolean: ResetPassword.tsx');
       }
       if (result.success) {
-        setShowSuccessPopup(true);
+        setPopupStates({...popupStates, showSuccessPopup: true});
         setTimeout(() => {
           navigate('/login');
         }, 3000);
       } else {
-        setShowErrorPopup(true);
+        setPopupStates({...popupStates, showErrorPopup: true});
       }
     } catch (err: unknown) {
       if (err instanceof Error) {
         logger.error(err.message);
       }
+      setPopupStates({...popupStates, showErrorPopup: true});
     }
-  }, [newPassword, confirmPassword, resetToken, comparePasswords, navigate]);
+  }, [newPassword, confirmPassword, resetToken, navigate, popupStates]);
 
   return (
     <div className={styles.container}>
@@ -124,31 +98,28 @@ const ResetPassword: FunctionComponent = () => {
         <h2>Reset Password</h2>
         <p>Enter New Password</p>
       </div>
-      {showMismatchPopup && (
+      {popupStates.showMismatchPopup && (
         <PopupMessage
           type={PopupType.Success}
           message="The passwords do not match"
-          onClick={showMismatchPopupFunction}
+          onClick={togglePopup('showMismatchPopup')}
         />
-      ) }
-      {showSuccessPopup && (
+      )}
+      {popupStates.showSuccessPopup && (
         <PopupMessage
           type={PopupType.Success}
           message="Password has been change, you will now be redirected to the login page"
-          onClick={showSuccessPopupFunction}
+          onClick={togglePopup('showSuccessPopup')}
         />
-      ) }
-      {showErrorPopup && (
+      )}
+      {popupStates.showErrorPopup && (
         <PopupMessage
           type={PopupType.Failure}
           message="Error: could not verify user credentials"
-          onClick={showErrorPopupFunction}
+          onClick={togglePopup('showErrorPopup')}
         />
-      ) }
-      <form
-        className={styles.newPassword}
-        onSubmit={handleSubmit}
-      >
+      )}
+      <form className={styles.newPassword} onSubmit={handleSubmit}>
         <div>
           <label>
             New Password:
@@ -156,7 +127,7 @@ const ResetPassword: FunctionComponent = () => {
               size={InputSize.Large}
               value={newPassword}
               setValue={setNewPassword}
-              onChange={handleNewPasswordChange}
+              onChange={handlePasswordChange(setNewPassword)}
               placeholderText="*********"
               type={showPassword ? 'text' : 'password'}
             />
@@ -169,35 +140,21 @@ const ResetPassword: FunctionComponent = () => {
               size={InputSize.Large}
               value={confirmPassword}
               setValue={setConfirmPassword}
-              onChange={handleConfirmPasswordChange}
+              onChange={handlePasswordChange(setConfirmPassword)}
               placeholderText="*********"
               type={showPassword ? 'text' : 'password'}
             />
           </label>
-          <label
-            htmlFor="check"
-            className={styles.passwordCheck}
-          >
+          <label htmlFor="check" className={styles.passwordCheck}>
             Show Password
-            {' '}
-            <input
-              id="check"
-              type="checkbox"
-              onChange={handleShowPasswordChange}
-            />
-
+            <input id="check" type="checkbox" onChange={handleShowPasswordChange} />
           </label>
-
         </div>
         <div className={styles.buttons}>
-          <Button
-            size={ButtonSize.Large}
-            type={ButtonType.Submit}
-          >
+          <Button size={ButtonSize.Large} type={ButtonType.Submit}>
             Reset Password
           </Button>
         </div>
-
       </form>
     </div>
   );
