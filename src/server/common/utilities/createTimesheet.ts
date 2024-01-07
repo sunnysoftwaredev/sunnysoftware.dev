f529b148ef579c2bae0fb5b1cc868428b0c729d0
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-01-07 06:07:28 +0000

    refactor(createTimesheet): implement comprehensive error handling

--- src/server/common/utilities/createTimesheet.ts
+++ src/server/common/utilities/createTimesheet.ts
@@ -1,17 +1,20 @@
 import { checkTimesheet, insertTimesheet } from '../../database';
 import logger from '../../logger';
 
-export const createTimesheet = async(
+export const createTimesheet = async (
   userId: number, weekStart: number,
   weekEnd: number
 ): Promise<void> => {
-  const recordExists = await checkTimesheet(userId, weekStart, weekEnd);
-  if (!recordExists) {
-    await insertTimesheet(userId, weekStart, weekEnd).catch((err) => {
-      if (err instanceof Error) {
-        logger.error(err.message);
-      }
-    });
+  try {
+    const recordExists = await checkTimesheet(userId, weekStart, weekEnd);
+    if (!recordExists) {
+      await insertTimesheet(userId, weekStart, weekEnd);
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      logger.error(`Error in createTimesheet for User ID ${userId}: ${err.message}`);
+    } else {
+      logger.error(`Unknown error in createTimesheet for User ID ${userId}`);
+    }
   }
 };
-
