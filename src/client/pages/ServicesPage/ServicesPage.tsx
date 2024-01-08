da7afb14ab67aba9279d957e62429f6b24f54b36
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-01-08 18:08:31 +0000

    refactor: use HelmetAsync in ServicesPage for async meta tag updates

--- src/client/pages/ServicesPage/ServicesPage.tsx
+++ src/client/pages/ServicesPage/ServicesPage.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import type { FunctionComponent } from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import ServicesPageBanner from '../../components/ServicesPage/ServicesPageBanner/ServicesPageBanner';
 import FullServicesList from '../../components/ServicesPage/FullServicesList/FullServicesList';
 import ServicesNeedHelp from '../../components/ServicesPage/ServicesNeedHelp/ServicesNeedHelp';
 
-const ServicesPage: FunctionComponent = () => (
-  <div>
-    <Helmet>
-      <meta charSet="utf-8" />
-      <title>Services-Sunny Software</title>
-      <link rel="canonical" href="https://sunnysoftware.dev/services" />
-      <meta
-        name="description"
-        content="Information on services offered by Sunny Software LLC"
-      />
-    </Helmet>
-    <ServicesPageBanner />
-    <FullServicesList />
-    <ServicesNeedHelp />
-  </div>
+const ServicesPage: React.FC = () => (
+  <HelmetProvider>
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Services-Sunny Software</title>
+        <link rel="canonical" href="https://sunnysoftware.dev/services" />
+        <meta
+          name="description"
+          content="Information on services offered by Sunny Software LLC"
+        />
+      </Helmet>
+      <ServicesPageBanner />
+      <FullServicesList />
+      <ServicesNeedHelp />
+    </div>
+  </HelmetProvider>
 );
 
 export default ServicesPage;
