cd9b1a31c06c1f1a03aa7fadd81ba18912662b5a
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-01-09 06:08:48 +0000

    refactor: Simplify type checking and preventDefault usage in useChangeHandler hook

--- src/client/hooks/useChangeHandler.ts
+++ src/client/hooks/useChangeHandler.ts
@@ -3,18 +3,14 @@ import type { ChangeEvent } from 'react';
 import logger from '../../server/logger';
 
 type SetterCallback = (value: string) => void;
-type ChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => void;
+type ChangeHandler = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
 
 const useChangeHandler = (setterCallback: SetterCallback): ChangeHandler => {
   const changeHandler: ChangeHandler = (e) => {
-    e.preventDefault();
-    const { target } = e;
-    if (target instanceof HTMLSelectElement) {
-      setterCallback(e.target.value);
-    } else {
-      logger.info('type error in TimeDropdown: handleValueChange');
-    }
+    const { value } = e.target;
+    setterCallback(value);
   };
+
   return useCallback(changeHandler, [setterCallback]);
 };
 
