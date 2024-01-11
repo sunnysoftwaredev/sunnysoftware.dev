bf7363f739fedf48f66eb894dc94a5360fdb8c48
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-01-11 06:08:45 +0000

    refactor: enhance error logging in 'main' function with error stack

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,13 +2,24 @@ import logger from './logger';
 import { initializeDatabase } from './database';
 import { initializeServer } from './server';
 
-const main = async(): Promise<void> => {
-  await initializeDatabase();
-  await initializeServer();
+const main = async (): Promise<void> => {
+  try {
+    await initializeDatabase();
+    await initializeServer();
+  } catch (e) {
+    if (e instanceof Error) {
+      logger.fatal(`Error during server startup: ${e.message}`);
+      logger.debug(e.stack);
+    } else {
+      logger.fatal('An unknown error occurred during server startup.');
+    }
+  }
 };
 
-main()
-  .catch((e) => {
-    logger.fatal('Error during server startup.');
-    logger.info(e);
-  });
+main().catch((e) => {
+  logger.fatal('Unhandled error caught in main execution.');
+  if (e instanceof Error) {
+    logger.error(e.message);
+    logger.debug(e.stack);
+  }
+});
