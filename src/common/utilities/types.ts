c0213636174dc5bdead8533b8a7fbb364de06317
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-01-12 12:11:16 +0000

    refactor: use every() in 'isIdArray' and 'isUsersArray' for consistent null-checking

--- src/common/utilities/types.ts
+++ src/common/utilities/types.ts
@@ -2,73 +2,66 @@ import type { EmployeeTimesheet, IdObject, UserIdNameEmailRole, ClientProject, T
 
 export const isObjectRecord
 = (value: unknown): value is Record<string, unknown> => (
-  typeof value === 'object'
-  && value !== null
-  && !Array.isArray(value)
+    typeof value === 'object'
+    && value !== null
+    && !Array.isArray(value)
 );
 
 export const isTimeObjectWithProject
  = (value: unknown): value is TimeObjectWithProject => (
-   isObjectRecord(value)
-  && 'unixStart' in value
-  && 'unixEnd' in value
-  && 'projectId' in value
+    isObjectRecord(value)
+    && 'unixStart' in value
+    && 'unixEnd' in value
+    && 'projectId' in value
  );
 
 export const isTimeObjectWithProjectArray
 = (value: unknown): value is TimeObjectWithProject[] => (
-  Array.isArray(value)
- && value.every(isTimeObjectWithProject)
+    Array.isArray(value)
+    && value.every(isTimeObjectWithProject)
 );
 
 export const isIdArray = (value: unknown): value is IdObject[] => (
-  Array.isArray(value)
-  && value[0] !== null
-  && value[0] !== undefined
-  && 'id' in value[0]
+    Array.isArray(value)
+    && value.every(val => val !== null && val !== undefined && 'id' in val)
 );
 
 const isEmployeeTimesheet
  = (currentValue: Record<string, unknown>): boolean => (
-   isObjectRecord(currentValue)
-  && 'hours' in currentValue
-  && 'submitted' in currentValue
-  && 'invoiced' in currentValue
-  && 'paid' in currentValue
-  && 'username' in currentValue
-  && 'email' in currentValue
- );
+    isObjectRecord(currentValue)
+    && 'hours' in currentValue
+    && 'submitted' in currentValue
+    && 'invoiced' in currentValue
+    && 'paid' in currentValue
+    && 'username' in currentValue
+    && 'email' in currentValue
+);
 
 export const isEmployeeTimesheetArray
  = (value: unknown): value is EmployeeTimesheet[] => (
-   Array.isArray(value)
-  && value.every(isEmployeeTimesheet)
- );
+    Array.isArray(value)
+    && value.every(isEmployeeTimesheet)
+);
 
 export const isUsersArray
  = (value: unknown): value is UserIdNameEmailRole[] => (
-   Array.isArray(value)
-   && value[0] !== null
-   && value[0] !== undefined
-    && 'id' in value[0]
-    && 'username' in value[0]
-    && 'email' in value[0]
-    && 'role' in value[0]
- );
+    Array.isArray(value)
+    && value.every(val => val !== null && val !== undefined && 'id' in val && 'username' in val && 'email' in val && 'role' in val)
+);
 
 const isClientProject
  = (currentValue: Record<string, unknown>): boolean => (
-   isObjectRecord(currentValue)
-  && 'id' in currentValue
-  && 'title' in currentValue
-  && 'description' in currentValue
-  && 'active' in currentValue
-  && 'username' in currentValue
-  && 'email' in currentValue
- );
+    isObjectRecord(currentValue)
+    && 'id' in currentValue
+    && 'title' in currentValue
+    && 'description' in currentValue
+    && 'active' in currentValue
+    && 'username' in currentValue
+    && 'email' in currentValue
+);
 
 export const isClientProjectArray
  = (value: unknown): value is ClientProject[] => (
-   Array.isArray(value)
-  && value.every(isClientProject)
- );
+    Array.isArray(value)
+    && value.every(isClientProject)
+);
