40a5a1c76d5deddf38c7c1f935a4c5c0a8a7d509
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-01-13 06:07:18 +0000

    refactor: Consolidate asset module declarations into single TypeScript statement

--- src/client/declaration.d.ts
+++ src/client/declaration.d.ts
@@ -1,34 +1,4 @@
-declare module '*.scss' {
-  const content: Record<string, string>;
-  export default content;
-}
-
-declare module '*.png' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.jpg' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.jpeg' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.svg' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.gif' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.ttf' {
+declare module '*.(scss|png|jpg|jpeg|svg|gif|ttf)' {
   const content: string;
   export default content;
 }
