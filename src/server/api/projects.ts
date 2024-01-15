da9439a62e36149fe4410057c7afe11159f403e1
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-01-15 06:10:17 +0000

    refactor: utilize middleware for request validation in project routes

--- src/server/api/projects.ts
+++ src/server/api/projects.ts
@@ -1,4 +1,4 @@
-import { Router as createRouter } from 'express';
+import { Router as createRouter, Request, Response, NextFunction } from 'express';
 import { Mutex } from 'async-mutex';
 import { isObjectRecord } from '../../common/utilities/types';
 import { createClientProject, getAllClientProjects, getIDWithToken, updateClientProject } from '../database';
@@ -7,13 +7,38 @@ import logger from '../logger';
 const router = createRouter();
 const mutex = new Mutex();
 
-router.get('/', (req, res) => {
+function validateRequestBody(req: Request, res: Response, next: NextFunction) {
+  if (!isObjectRecord(req.body)) {
+    res.status(400).json({ success: false, error: 'Request body is not an object' });
+  } else {
+    next();
+  }
+}
+
+function validateCookies(req: Request, res: Response, next: NextFunction) {
+  if (!isObjectRecord(req.cookies)) {
+    res.status(400).json({ success: false, error: 'Cookies are not an object' });
+  } else {
+    next();
+  }
+}
+
+function validateAuthenticationToken(req: Request, res: Response, next: NextFunction) {
+  const { authenticationToken } = req.cookies;
+  if (typeof authenticationToken !== 'string') {
+    res.status(400).json({ success: false, error: 'Authentication token must be a string' });
+  } else {
+    next();
+  }
+}
+
+router.get('/', [validateRequestBody, validateCookies, validateAuthenticationToken], (req, res) => {
   (async(): Promise<void> => {
     const release = await mutex.acquire();
     try {
       const projects = await getAllClientProjects();
       if (typeof projects !== 'object') {
-        throw new Error('api/projects: no projects found');
+        throw new Error('No projects found');
       }
       res.json({
         success: true,
@@ -24,113 +49,64 @@ router.get('/', (req, res) => {
     }
     logger.debug('res.json success in api/projects.get');
   })().catch((e: Error) => {
-    res.json({
+    res.status(500).json({
       success: false,
       error: e.message,
     });
   });
 });
 
-router.post('/', (req, res) => {
+router.post('/', [validateRequestBody, validateCookies, validateAuthenticationToken], (req, res) => {
   (async(): Promise<void> => {
-    if (!isObjectRecord(req.body)) {
-      throw new Error('api/projects: req.body is not object');
-    }
     const { client, title, description } = req.body;
 
-    if (!isObjectRecord(req.cookies)) {
-      throw new Error('api/projects: req.cookies is not object');
-    }
-
     const { authenticationToken } = req.cookies;
-    if (typeof authenticationToken !== 'string') {
-      throw new Error('api/projects: userToken not type string');
-    }
-
-    if (typeof client !== 'string') {
-      throw new Error('api/projects.post: client is not string');
-    }
-    if (typeof title !== 'string') {
-      throw new Error('api/projects.post: title is not string');
-    }
-    if (typeof description !== 'string') {
-      throw new Error('api/projects.post: description is not string');
-    }
-
+    
+    const id = Number(client);
     const release = await mutex.acquire();
     try {
       const idResult = getIDWithToken(authenticationToken);
       if (typeof idResult !== 'object') {
-        throw new Error('api/projects: no idResult found');
+        throw new Error('No idResult found');
       }
 
-      const id = Number(client);
       await createClientProject(id, title, description);
 
-      res.json({
-        success: true,
-      });
+      res.json({ success: true });
     } finally {
       release();
     }
-
     logger.debug('res.json success in api/projects.post');
   })().catch((e: Error) => {
-    res.json({
+    res.status(500).json({
       success: false,
       error: e.message,
     });
   });
 });
 
-router.put('/', (req, res) => {
+router.put('/', [validateRequestBody, validateCookies, validateAuthenticationToken], (req, res) => {
   (async(): Promise<void> => {
-    if (!isObjectRecord(req.body)) {
-      throw new Error('api/projects: req.body is not object');
-    }
     const { id, newTitle, newDescription, newActive } = req.body;
 
-    if (!isObjectRecord(req.cookies)) {
-      throw new Error('api/projects: req.cookies is not object');
-    }
-
     const { authenticationToken } = req.cookies;
-    if (typeof authenticationToken !== 'string') {
-      throw new Error('api/projects: userToken not type string');
-    }
-
-    if (typeof id !== 'number') {
-      throw new Error('api/projects.put: id is not number');
-    }
-    if (typeof newTitle !== 'string') {
-      throw new Error('api/projects.put: title is not string');
-    }
-    if (typeof newDescription !== 'string') {
-      throw new Error('api/projects.put: description is not string');
-    }
-    if (typeof newActive !== 'boolean') {
-      throw new Error('api/projects.put: active is not boolean');
-    }
-
+    
     const release = await mutex.acquire();
     try {
       const idResult = getIDWithToken(authenticationToken);
       if (typeof idResult !== 'object') {
-        throw new Error('api/projects: no idResult found');
+        throw new Error('No idResult found');
       }
 
       await updateClientProject(id, newTitle, newDescription, newActive);
 
-      res.json({
-        success: true,
-      });
+      res.json({ success: true });
     } finally {
       release();
     }
-
-    logger.debug('res.json success in api/projects.post');
+    logger.debug('res.json success in api/projects.put');
   })().catch((e: Error) => {
-    res.json({
+    res.status(500).json({
       success: false,
       error: e.message,
     });
