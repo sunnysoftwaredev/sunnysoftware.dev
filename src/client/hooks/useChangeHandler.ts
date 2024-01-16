bc67a1aa60e9f7837c6e8c9e6bf1576333fe283b
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-01-16 12:11:19 +0000

    refactor: Simplify element type check in useChangeHandler hook

--- src/client/hooks/useChangeHandler.ts
+++ src/client/hooks/useChangeHandler.ts
@@ -3,17 +3,11 @@ import type { ChangeEvent } from 'react';
 import logger from '../../server/logger';
 
 type SetterCallback = (value: string) => void;
-type ChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => void;
 
-const useChangeHandler = (setterCallback: SetterCallback): ChangeHandler => {
-  const changeHandler: ChangeHandler = (e) => {
+const useChangeHandler = (setterCallback: SetterCallback) => {
+  const changeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
     e.preventDefault();
-    const { target } = e;
-    if (target instanceof HTMLSelectElement) {
-      setterCallback(e.target.value);
-    } else {
-      logger.info('type error in TimeDropdown: handleValueChange');
-    }
+    setterCallback(e.target.value);
   };
   return useCallback(changeHandler, [setterCallback]);
 };
