2d9d0a487703e556fba66765ae449f438e088257
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-01-16 12:11:06 +0000

    refactor: simplify  with promisify for better readability

--- src/server/common/utilities/crypto.ts
+++ src/server/common/utilities/crypto.ts
@@ -1,4 +1,5 @@
-import crypt from 'crypto';
+import crypt, { randomBytes } from 'crypto';
+import { promisify } from 'util';
 
 // TODO: determine an appropriate number
 const HASH_ROUNDS = 500;
@@ -7,16 +8,8 @@ export const hash = (input: Buffer): Buffer => {
   const result = crypt.createHash('sha256').update(input).digest();
   return result;
 };
-// changed from 256 to 128: salt.toString(hex) returns 512 char
-export const generateSalt
-= async(): Promise<Buffer> => new Promise((resolve, reject) => {
-  crypt.randomBytes(128, (err, buf) => {
-    if (err !== null) {
-      reject(err);
-    }
-    resolve(buf);
-  });
-});
+// Use promisify to simplify Promise-based usage of crypto.randomBytes
+export const generateSalt = promisify(randomBytes);
 
 export const saltAndHash = (password: string, salt: Buffer): Buffer => {
   const bufferPassword = Buffer.from(password);
