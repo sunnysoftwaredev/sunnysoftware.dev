e38ccaaf62dc3c455b3ec62dd69395e28ab80d21
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-01-19 06:08:46 +0000

    refactor: externalize faqData from FrequentlyAskedQuestions component

--- src/client/components/FrequentlyAskedQuestions/FrequentlyAskedQuestions.tsx
+++ src/client/components/FrequentlyAskedQuestions/FrequentlyAskedQuestions.tsx
@@ -3,34 +3,9 @@ import React from 'react';
 import AccordianPanel from '../AccordianPanel/AccordianPanel';
 import Button, { ButtonSize } from '../Button/Button';
 import styles from './FrequentlyAskedQuestions.scss';
+import faqData from './faqData'; // Added import for externalized FAQ data
 
 const FrequentlyAskedQuestions: FunctionComponent = () => {
-  const faqData = [
-    {
-      question: 'Question 1',
-      answer: `You and your team can request design work through a
-      shared board we will set up for you in Asana. You can request as
-      many designs as you like.`,
-    },
-    {
-      question: 'Question 2',
-      answer: `You and your team can request design work through a
-      shared board we will set up for you in Asana. You can request as
-      many designs as you like.`,
-    },
-    {
-      question: 'Question 3',
-      answer: `You and your team can request design work through a
-      shared board we will set up for you in Asana. You can request as
-      many designs as you like.`,
-    },
-    {
-      question: 'Question 4',
-      answer: `You and your team can request design work through a
-      shared board we will set up for you in Asana. You can request as
-      many designs as you like.`,
-    },
-  ];
   return (
     <div className={styles.faqContainer}>
       <h2>FAQ</h2>
