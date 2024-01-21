22e3b6a43cf61fd8d58afb1f34ea95265507563c
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-01-21 00:32:04 +0000

    refactor: rename 'IProps' to 'AdminPrivateRouteProps' for clarity

--- src/client/components/PrivateRoutes/AdminPrivateRoute.tsx
+++ src/client/components/PrivateRoutes/AdminPrivateRoute.tsx
@@ -3,11 +3,11 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { getIsAdmin } from '../../redux/selectors/account';
 
-interface IProps {
+interface AdminPrivateRouteProps {
   children: React.JSX.Element;
 }
 
-const AdminPrivateRoute = ({ children }: IProps): React.JSX.Element => {
+const AdminPrivateRoute = ({ children }: AdminPrivateRouteProps): React.JSX.Element => {
   const isAdmin = useSelector(getIsAdmin);
   if (!isAdmin) {
     return <Navigate to="/login" />;
