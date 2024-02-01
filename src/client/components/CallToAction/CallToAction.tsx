b68cc8604f1caaf90a8fcd58f2454156ff14c78e
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-02-01 00:28:53 +0000

    refactor: Change  to named function for better readability & testability

--- src/client/components/CallToAction/CallToAction.tsx
+++ src/client/components/CallToAction/CallToAction.tsx
@@ -3,21 +3,23 @@ import type { FunctionComponent } from 'react';
 import Button, { ButtonSize } from '../Button/Button';
 import style from './CallToAction.scss';
 
-const CallToAction: FunctionComponent = () => (
-  <div className={style.ctaContainer}>
-    <h2>We make your business shine</h2>
-    <p>
-      Book a call to see how Perspective Design
-      can help elevate your company&#39;s designs
-      to the next level.
-    </p>
-    <Button
-      size={ButtonSize.Large}
-      to="/contact-us"
-    >
-      Explore more
-    </Button>
-  </div>
-);
+function CallToAction() {
+  return (
+    <div className={style.ctaContainer}>
+      <h2>We make your business shine</h2>
+      <p>
+        Book a call to see how Perspective Design
+        can help elevate your company's designs
+        to the next level.
+      </p>
+      <Button
+        size={ButtonSize.Large}
+        to="/contact-us"
+      >
+        Explore more
+      </Button>
+    </div>
+  );
+}
 
 export default CallToAction;
