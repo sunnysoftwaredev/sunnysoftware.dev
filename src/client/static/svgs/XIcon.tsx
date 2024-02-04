4d5e5898a42a345261a6d50a13ae74db722e2a05
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-02-04 18:01:17 +0000

    refactor: specify empty props type for XIcon component

--- src/client/static/svgs/XIcon.tsx
+++ src/client/static/svgs/XIcon.tsx
@@ -1,7 +1,9 @@
 import type { FunctionComponent } from 'react';
 import React from 'react';
 
-const XIcon: FunctionComponent = () => (
+interface Props {}
+
+const XIcon: FunctionComponent<Props> = () => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M6 18L18 6M6 6L18 18" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
   </svg>
