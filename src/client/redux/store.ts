27e5dcd3aff114c08ee7355ffc7189a06e62a27e
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-02-06 00:02:42 +0000

    refactor: Add getPreloadedState function for safe access to window.__INITIAL_STATE__

--- src/client/redux/store.ts
+++ src/client/redux/store.ts
@@ -5,17 +5,18 @@ export type State = ReturnType<typeof reducer>;
 
 declare global {
   interface Window {
-    // eslint-disable-next-line id-match
-    __INITIAL_STATE__: State;
+    __INITIAL_STATE__?: State;
   }
 }
 
-// eslint-disable-next-line no-underscore-dangle
-const preloadedState = window.__INITIAL_STATE__;
+// Safely retrieves preloaded state from global window object
+const getPreloadedState = (): State | undefined => {
+  return window.__INITIAL_STATE__;
+}
 
 const store = configureStore({
   reducer,
-  preloadedState,
+  preloadedState: getPreloadedState(),
 });
 
 export default store;
