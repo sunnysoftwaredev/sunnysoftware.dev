1d8945c8d09317639024153573e429828bf1dedd
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-02-09 06:01:24 +0000

    refactor: standardize React imports in AboutUsPage.tsx for consistency

--- src/client/pages/AboutUsPage/AboutUsPage.tsx
+++ src/client/pages/AboutUsPage/AboutUsPage.tsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import type { FunctionComponent } from 'react';
+import React, { FunctionComponent } from 'react';
 import { Helmet } from 'react-helmet';
-import AboutUsTop from '../../components/AboutUsPage/AboutUsTop/AboutUsTop';
-import StatisticsBar from '../../components/AboutUsPage/StatisticsBar/StatisticsBar';
-import MissionAndVision from '../../components/AboutUsPage/MissionAndVision/MissionAndVision';
-import CompanyTimeline from '../../components/AboutUsPage/CompanyTimeline/CompanyTimeline';
-import groupPhoto from '../../components/AboutUsPage/aboutUsGroupPhoto.png';
-import BuildAndGrow from '../../components/AboutUsPage/BuildAndGrow/BuildAndGrow';
-import LocationBanner from '../../components/AboutUsPage/LocationBanner/LocationBanner';
-import OurTeamAndOpenings from '../../components/OurTeamAndOpenings/OurTeamAndOpenings';
+
+// All page components are now imported from components/AboutUsPage/index.ts which exports them
+import {
+  AboutUsTop,
+  StatisticsBar,
+  MissionAndVision,
+  CompanyTimeline,
+  BuildAndGrow,
+  LocationBanner,
+  OurTeamAndOpenings,
+} from '../../components/AboutUsPage';
+
 import CallToAction from '../../components/CallToAction/CallToAction';
+import groupPhoto from '../../components/AboutUsPage/aboutUsGroupPhoto.png';
 import styles from './AboutUsPage.scss';
 
 const AboutUsPage: FunctionComponent = () => (
   <div>
     <Helmet>
       <meta charSet="utf-8" />
-      <title>About Us-Sunny Software</title>
+      <title>About Us - Sunny Software</title>
       <link rel="canonical" href="https://sunnysoftware.dev/about-us" />
       <meta name="description" content="Information about Sunny Software LLC" />
     </Helmet>
