f4eab1cf674227edcb9ebd29ed7533e80544bd9b
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-02-10 18:01:15 +0000

    refactor: rename 'setterCallback' to 'setValue' in 'useChangeHandler' hook for better clarity

--- src/client/hooks/useChangeHandler.ts
+++ src/client/hooks/useChangeHandler.ts
@@ -2,20 +2,20 @@ import { useCallback } from 'react';
 import type { ChangeEvent } from 'react';
 import logger from '../../server/logger';
 
-type SetterCallback = (value: string) => void;
+type SetValue = (value: string) => void;
 type ChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => void;
 
-const useChangeHandler = (setterCallback: SetterCallback): ChangeHandler => {
+const useChangeHandler = (setValue: SetValue): ChangeHandler => {
   const changeHandler: ChangeHandler = (e) => {
     e.preventDefault();
     const { target } = e;
     if (target instanceof HTMLSelectElement) {
-      setterCallback(e.target.value);
+      setValue(target.value);
     } else {
       logger.info('type error in TimeDropdown: handleValueChange');
     }
   };
-  return useCallback(changeHandler, [setterCallback]);
+  return useCallback(changeHandler, [setValue]);
 };
 
 export default useChangeHandler;
