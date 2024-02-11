37e17922e261fe644074c4f18df23061be3002f7
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-02-11 06:01:25 +0000

    refactor(accordian-panel): use functional update for 'setIsOpen' in 'togglePanel'

--- src/client/components/AccordianPanel/AccordianPanel.tsx
+++ src/client/components/AccordianPanel/AccordianPanel.tsx
@@ -7,24 +7,27 @@ type AccordianPanelProps = {
   answer: string;
 };
 
-const AccordianPanel: FunctionComponent<AccordianPanelProps>
- = ({ question, answer }) => {
-   const [isOpen, setIsOpen] = useState(false);
-   const togglePanel = useCallback(() => {
-     setIsOpen(!isOpen);
-   }, [isOpen]);
-   return (
-     <div className={styles.accordianPanelContainer}>
-       <div className={styles.accordianQuestion} onClick={togglePanel}>
-         <h3>{question}</h3>
-         <div className={styles.panelArrow}>
-           {'>'}
-         </div>
-       </div>
-       <div className={styles.accordianDivider} />
-       {isOpen && <p className={styles.accordianAnswer}>{answer}</p>}
-     </div>
-   );
- };
+const AccordianPanel: FunctionComponent<AccordianPanelProps> = ({ question, answer }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Using the functional update form to negate the previous state
+  // removes the necessity of dependency array for useCallback
+  const togglePanel = useCallback(() => {
+    setIsOpen(open => !open);
+  }, []);
+
+  return (
+    <div className={styles.accordianPanelContainer}>
+      <div className={styles.accordianQuestion} onClick={togglePanel}>
+        <h3>{question}</h3>
+        <div className={styles.panelArrow}>
+          {'>'}
+        </div>
+      </div>
+      <div className={styles.accordianDivider} />
+      {isOpen && <p className={styles.accordianAnswer}>{answer}</p>}
+    </div>
+  );
+};
 
 export default AccordianPanel;
